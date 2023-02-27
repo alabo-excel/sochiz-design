f7b34a90637e20c3e0af5ae0c9c03fdc9f2f3a43
alabo-excel <iamalaboexcel@gmail.com>
2023-02-26 15:08:30 -1100

    fix: slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,79 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      //   modules={[Navigation]}
+      //   navigation
+      breakpoints={{
+        340: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+    >
+      <SwiperSlide>
+        <div className="p-3 py-8 border border-[#FFFFFF33] rounded-md">
+          <img src="./stop.png" alt="" />
+          <h1 className="text-[#FAD646] font-bold text-2xl my-3">
+            Product Design
+          </h1>
+          <p className="text-sm text-white">
+            Learn about the user experience (UX) industry and explore the basic
+            concepts and roles within the UX field.
+          </p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="p-3 py-8 border border-[#FFFFFF33] rounded-md">
+          <img src="./stop.png" alt="" />
+          <h1 className="text-[#FAD646] font-bold text-2xl my-3">
+            Product Design
+          </h1>
+          <p className="text-sm text-white">
+            Learn about the user experience (UX) industry and explore the basic
+            concepts and roles within the UX field.
+          </p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="p-3 py-8 border border-[#FFFFFF33] rounded-md">
+          <img src="./stop.png" alt="" />
+          <h1 className="text-[#FAD646] font-bold text-2xl my-3">
+            Product Design
+          </h1>
+          <p className="text-sm text-white">
+            Learn about the user experience (UX) industry and explore the basic
+            concepts and roles within the UX field.
+          </p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="p-3 py-8 border border-[#FFFFFF33] rounded-md">
+          <img src="./stop.png" alt="" />
+          <h1 className="text-[#FAD646] font-bold text-2xl my-3">
+            Creative Dairy{" "}
+          </h1>
+          <p className="text-sm text-white">
+            Learn about the user experience (UX) industry and explore the basic
+            concepts and roles within the UX field.
+          </p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,35 @@
-import HeaderNav from "../components/HeaderNav";
-// import { Inter } from 'next/font/google'
-
-// const inter = Inter({ subsets: ['latin'] })
-
+import Slider from "@/components/Slider";
+import HeaderNav from "@/components/HeaderNav";
 export default function Home() {
   return (
     <>
       <HeaderNav />
       <main>
-        <div className="p-20">
-          <div className=" w-[32%] text-center mx-auto">
+        <div className="lg:p-20 sm:py-14 md:p-8">
+          <div className="lg:w-[32%] text-center mx-auto">
             <div className="text-3xl font-black capitalize text-white">
               Best in Design, animation and{" "}
               <span className="text-[#FEAE1B]">Creative Diary</span>{" "}
             </div>
             <div className="mt-8">
-              <button className="bg-[#1C2128] p-3 rounded-md text-white w-40 mx-2">
+              <button className="bg-[#1C2128] p-3 rounded-md text-white w-40 sm:w-80 sm:my-2 mx-2">
                 Our Services
               </button>
-              <button className="bg-[#FEAE1B] p-3 rounded-md mx-2 w-40">
+              <button className="bg-[#FEAE1B] p-3 rounded-md mx-2 w-40  sm:w-80 sm:my-2">
                 Contact Us
               </button>
             </div>
           </div>
+        </div>
+        <div className="lg:w-[80%] sm:w-[85%] mx-auto">
+          <div className="flex justify-evenly mx-auto w-96 py-8">
+            <div className="my-auto">
+              <div className="p-1 w-20 bg-white"></div>
+              <div className="p-1 w-20 bg-white mt-1"></div>
+            </div>
+            <div className="text-white font-black text-4xl my-auto">Our Services</div>
+          </div>
+          <Slider />
         </div>
       </main>
     </>
