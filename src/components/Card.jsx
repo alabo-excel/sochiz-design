cba374ca404fbba1114cf7d5976714d2214e27e5
alabo-excel <iamalaboexcel@gmail.com>
2023-02-28 01:02:53 -1100

    fix: animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Fade, Slide } from "react-awesome-reveal";
 const Card = ({ img, title }) => {
   return (
     <div className="rounded-md my-3 w-[30%] sm:w-full">
-      <Fade delay={1e3}>
+      <Fade delay={1e3} cascade>
         <img className="w-full h-52 object-cover" src={img} alt="" />
         <div className="p-3 border border-[#FFFFFF33]">
           <h2 className="text-base sm:text-base text-white">{title}</h2>
